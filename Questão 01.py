7ef7037f85a23caeb8c47a78b8bbf6cd0c877476
DaniloKorsakov <55713861+DaniloKorsakov@users.noreply.github.com>
2019-09-28 00:58:34 -0300

    Add files via upload

---
+++ Questão 01.py
@@ -0,0 +1,6 @@
+# Questão 01
+preco = total = 0
+for preco in range (10):
+    preco = float(input('Valor: '))
+    total += preco
+print ('Total: {}'.format(total))
---
+++ Questão 03.py
@@ -0,0 +1,6 @@
+# Questão 03
+idade = total = 0
+for idade in range (10):
+    idade = int(input('Valor: '))
+    total += idade / 10
+print ('Total: {}'.format(total))
